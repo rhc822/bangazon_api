4dd43679adf9a4aca05c2dab038b7efd18315914
Joe Shepherd <joeshepmedia@gmail.com>
2020-02-26 10:49:49 -0600

    Remove api app, create new version

--- bangazon/settings.py
+++ bangazon/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bangazon_customer_api.urls'
+ROOT_URLCONF = 'bangazon.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'bangazon_customer_api.wsgi.application'
+WSGI_APPLICATION = 'bangazon.wsgi.application'
 
 
 # Database
