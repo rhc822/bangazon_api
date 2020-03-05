3f41c5d98b6a809a873d16f4f3ca7b1c6ce60df3
rhc822 <rhc822@gmail.com>
2020-03-05 10:21:41 -0600

    Created fixtures for order, payment types, and products. Created or updated models for Order.py and payment_type.py. Created or updated views for orders.py and payment_types.py

--- bangazon_customer_api/views/__init__.py
+++ bangazon_customer_api/views/__init__.py
@@ -2,3 +2,5 @@ from .register import register_user
 from .register import login_user
 from .product_types import ProductTypes
 from .products import Products
+from .payment_types import PaymentTypes
+from .orders import Orders
--- bangazon_customer_api/views/orders.py
+++ bangazon_customer_api/views/orders.py
@@ -1,2 +1,53 @@
+"""View module for handling requests about orders"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from bangazon_customer_api.models import Order
 
-    
+
+class OrdersSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for Orders
+
+    Arguments:
+        serializers.HyperlinkedModelSerializer
+    """
+    class Meta:
+        model = Order
+        url = serializers.HyperlinkedIdentityField(
+            view_name='orders',
+            lookup_field='id'
+        )
+        fields = ('id', 'created_at', 'customer_id', 'payment_type_id')
+
+
+class Orders(ViewSet):
+    """Orders for Bangazon"""
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single Order
+
+        Returns:
+            Response -- JSON serialized order instance
+        """
+        try:
+            area = Order.objects.get(pk=pk)
+            serializer = OrdersSerializer(area, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def list(self, request):
+        """Handle GET requests to Order resource
+
+        Returns:
+            Response -- JSON serialized list of Order
+        """
+        areas = Order.objects.all()
+        serializer = OrdersSerializer(
+            areas,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
